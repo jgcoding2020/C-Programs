d928dc6880fabb0c9ad2acbcae61f5c33e73d9bc
Joshua David Gardner <76405317+jgcoding2020@users.noreply.github.com>
2022-12-30 13:44:30 -0500

    Update Joshua_Gardner_Lab4.c

--- Joshua_Gardner_Lab4.c
+++ Joshua_Gardner_Lab4.c
@@ -1,8 +1,6 @@
-/*
-Student: Joshua Gardner Z15117842
+/*Author: Joshua Gardner
 Lab 4: Use of functions along with for loops, while loops, and arrays
-Date: 7/11/2020
-*/
+Date: 7/11/2020*/
 
 #include <stdio.h>
 #include <stdlib.h>
